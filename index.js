2f942335237dba30420709bd0df56bf2ad1abd6c
Anastasiia Sereda <nastiasereda.1994@gmail.com>
2021-01-28 17:10:29 +0200

    done with this task

--- index.js
+++ index.js
@@ -1,41 +1,12 @@
-const argv = require("yargs").argv;
-const cont = require("./contacts");
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const routes = require("./routes/contactsRouter");
 
-// console.log('test',cont.listContacts());
-// cont.listContacts();
-// cont.removeContact(2);
-// cont.addContact("testd", "testg", 1234);
-// console.log(path.join(__dirname, "./db", "contacts.json"));
-// exports.contactsPath = contactsPath;
-function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-    case "list":
-      cont.listContacts().then((data) => {
-        console.table(data);
-      });
-      break;
-
-    case "get":
-      cont.getContactById(id).then((data) => {
-        console.table(data);
-      });
-      break;
-
-    case "add":
-      cont.addContact(name, email, phone).then((data) => {
-        console.table(data);
-      });
-      break;
-
-    case "remove":
-      cont.removeContact(id).then((data) => {
-        console.table(data);
-      });
-      break;
-
-    default:
-      console.warn("\x1B[31m Unknown action type!");
-  }
-}
-
-invokeAction(argv);
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan("dev"));
+app.use("/api/contacts", routes);
+app.listen(3000);
---
+++ models/contacts.js
@@ -0,0 +1,95 @@
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+const util = require("util");
+const fs = require("fs");
+const writeFilePromisified = util.promisify(fs.writeFile);
+const readFilePromise = util.promisify(fs.readFile);
+const contactsPath = path.join("./db", "contacts.json");
+
+async function listContacts() {
+  const data = await readFilePromise(contactsPath);
+  return JSON.parse(data);
+}
+
+async function getContactById(contactId) {
+  const data = await listContacts();
+  return data.filter(({ id }) => id.toString() === contactId);
+}
+async function removeContact(contactId) {
+  try {
+    const data = await listContacts();
+    const infData = data.filter(({ id }) => id.toString() === contactId);
+    const newArr = data.filter(({ id }) => id.toString() !== contactId);
+    await writeFilePromisified(contactsPath, JSON.stringify(newArr));
+    if (infData.length > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const data = await listContacts();
+    const newObj = {
+      id: uuidv4(),
+      name,
+      email,
+      phone,
+    };
+    const arrNew = [newObj, ...data];
+    await writeFilePromisified(contactsPath, JSON.stringify(arrNew));
+    return newObj;
+  } catch (err) {
+    console.log(err);
+  }
+}
+async function updateContact(id, body) {
+  try {
+    if (
+      body.name === undefined &&
+      body.email === undefined &&
+      body.phone === undefined
+    ) {
+      return { inf: false, message: "missing fields" };
+    } else {
+      const data = await listContacts();
+      const idx = data.findIndex((el) => {
+        return el.id.toString() === id;
+      });
+      if (idx === -1) {
+        return { inf: false, message: "Not found" };
+      } else {
+        let foundObj = data[idx];
+        function setData(dataNew, dataOld) {
+          if (dataNew === undefined || dataNew.trim() === "") {
+            return dataOld;
+          } else {
+            return dataNew;
+          }
+        }
+
+        const setObj = {
+          id: foundObj.id,
+          name: setData(body.name, foundObj.name),
+          email: setData(body.email, foundObj.email),
+          phone: setData(body.phone, foundObj.phone),
+        };
+        const newArr = [...data.slice(0, idx), setObj, ...data.slice(idx + 1)];
+
+        await writeFilePromisified(contactsPath, JSON.stringify(newArr));
+        return { inf: true, obj: setObj };
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+exports.listContacts = listContacts;
+exports.getContactById = getContactById;
+exports.removeContact = removeContact;
+exports.addContact = addContact;
+exports.updateContact = updateContact;
